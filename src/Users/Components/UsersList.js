418da0289ad5512f56ae6e139e6ef5e0e9f719ab
Hasham <hasham.hasham1@gmail.com>
2020-07-27 14:04:53 +0500

    Userlist added

---
+++ src/Users/Components/UsersList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import UserItem from "./UserItem";
+import "./UsersList.css";
+const UsersList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className={"center"}>
+        <h4>404 Users Not Found</h4>
+      </div>
+    );
+  }
+
+  return (
+    <ul className={"users-list"}>
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  );
+};
+export default UsersList;
--- src/Users/Pages/Users.js
+++ src/Users/Pages/Users.js
@@ -1,9 +1,18 @@
 import React from "react";
-
+import UsersList from "../Components/UsersList";
 const Users = (props) => {
+  const USER = [
+    {
+      id: "3",
+      name: "Hasham",
+      image:
+        "https://1.bp.blogspot.com/-FeykZDeyk_g/UqrKmAthznI/AAAAAAAAHT0/idOubv97yUk/s4000/Taylor+Swift+special+pictures.singersceleb.blogspot-1048.jpg",
+      places: 4,
+    },
+  ];
   return (
     <div>
-      <h3>Users component</h3>
+      <UsersList items={USER} />
     </div>
   );
 };
