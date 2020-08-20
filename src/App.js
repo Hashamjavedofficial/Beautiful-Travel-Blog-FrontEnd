7130e0c4c123d106d5e1fe584b86b09fffb89bf6
Hasham Javed <hasham.hasham1@gmail.com>
2020-08-20 10:36:52 +0500

    expiration logout working

--- src/App.js
+++ src/App.js
@@ -14,28 +14,54 @@ import Auth from "./user/pages/Auth";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import { AuthContext } from "./shared/context/auth-context";
 
+let logoutTimer;
 const App = () => {
+  const [expireTime, setExpireTime] = useState(null);
   const [token, setToken] = useState(null);
   const [userId, setUserId] = useState(false);
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, previousTokenExpiration) => {
     setToken(token);
     setUserId(uid);
-    localStorage.setItem("userData", JSON.stringify({ userId: uid, token }));
+    const tokenExpiration =
+      previousTokenExpiration ||
+      new Date(new Date().getTime() + 1000 * 60 * 60);
+    setExpireTime(tokenExpiration);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token,
+        expiration: tokenExpiration,
+      })
+    );
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
+    setExpireTime(null);
     setUserId(false);
     localStorage.removeItem("userData");
   }, []);
 
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem("userData"));
-    if (storedData && storedData.token) {
-      login(storedData.userId, storedData.token);
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(storedData.userId, storedData.token, storedData.expiration);
     }
   }, [login]);
+  useEffect(() => {
+    if (token && expireTime) {
+      const remainingTime = expireTime.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, expireTime]);
 
   let routes;
   if (token) {
