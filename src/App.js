dceae8a2ce8ffb6b1662fcdc77b851c4b6261b72
Hasham Javed <hasham.hasham1@gmail.com>
2020-08-21 23:12:33 +0500

    span added with test text

--- src/App.js
+++ src/App.js
@@ -76,6 +76,7 @@ const App = () => {
             fallback={
               <div className={"center"}>
                 <LoadingSpinner />
+                <span>as;ldka;slkd</span>
               </div>
             }
           >
